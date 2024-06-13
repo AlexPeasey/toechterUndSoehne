37018f481614762cec57f844890084f75c06a750
Alex Peasey <2>
2024-06-13 12:16:42 +0200

    Removed old code, updated age filters

--- summerschoolsintern.js
+++ summerschoolsintern.js
@@ -8,52 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // Initialize the datepicker
-  const datepickers = document.querySelectorAll('[data-toggle="datepicker"]');
-  datepickers.forEach((datepicker) => {
-    new Datepicker(datepicker, {
-      format: "dd.mm.yyyy",
-      autoHide: true,
-      language: "de-DE",
-    });
-  });
-
-  // Set input field to readonly on smaller screens
-  if (window.innerWidth < 768) {
-    datepickers.forEach((datepicker) => {
-      datepicker.setAttribute("readonly", "readonly");
-    });
-  }
-
-  // Parse date string in DD.MM.YYYY format
-  function parseDate(dateString) {
-    let parts = dateString.split(".");
-    return new Date(parts[2], parts[1] - 1, parts[0]);
-  }
-
-  // Function to filter items based on the date
-  function filterItems(pickedDate) {
-    let items = document.querySelectorAll(".summer-schools-item.w-dyn-item");
-
-    items.forEach((item) => {
-      const startDate = parseDate(item.querySelector(".start-date").innerText);
-      const endDate = parseDate(item.querySelector(".end-date").innerText);
-      const shouldBeVisible = pickedDate >= startDate && pickedDate <= endDate;
-
-      if (shouldBeVisible && item.classList.contains("fade-out")) {
-        item.style.display = ""; // Make it visible in the flow
-        requestAnimationFrame(() => {
-          item.classList.remove("fade-out"); // Start fade-in transition
-        });
-      } else if (!shouldBeVisible && !item.classList.contains("fade-out")) {
-        item.classList.add("fade-out"); // Start fade-out transition
-        setTimeout(() => {
-          item.style.display = "none"; // Completely hide after transition
-        }, 100); // match the duration of the CSS transition
-      }
-    });
-  }
-
   // Function to filter items based on age
   function filterAge(age) {
     let items = document.querySelectorAll(".summer-schools_list .w-dyn-item");
@@ -97,117 +51,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  const countItems = () => {
-    let items = document.querySelectorAll(".summer-schools-item:not(.fade-out)");
-    document.querySelector(".summer-schools_results-amount.filtered").textContent = items.length;
-  };
-
-  const updateAndShowDateTag = () => {
-    const dateTag = document.querySelector(".summer-schools_date-filter-tag");
-    const dateText = document.querySelector(".date-tag-text");
-    const date = document.querySelector(".summer-schools_input.w-input").value;
-    dateText.textContent = `Datum: ${date}`;
-    dateTag.style.display = "block";
-  };
-
-  const updateAndShowAgeTag = (age) => {
-    const ageTag = document.querySelector(".summer-schools_age-filter-tag");
-    const ageText = document.querySelector(".age-tag-text");
-    ageText.textContent = `Alter: ${age}`;
-    ageTag.style.display = "block";
-  };
-
-  const removeDateFilter = () => {
-    const dateTag = document.querySelector(".summer-schools_date-filter-tag");
-    dateTag.style.display = "none";
-    datepickers.forEach((datepicker) => {
-      datepicker.datepicker("reset");
-    });
-    resetItemsVisibility();
-  };
-
   const removeAgeFilter = () => {
-    const ageTag = document.querySelector(".summer-schools_age-filter-tag");
-    ageTag.style.display = "none";
     document.querySelector(".age-select").selectedIndex = 0;
-    resetItemsVisibility();
-  };
-
-  window.fsAttributes = window.fsAttributes || [];
-  window.fsAttributes.push([
-    "cmsfilter",
-    (filterInstances) => {
-      const [filterInstance] = filterInstances;
-      filterInstance.listInstance.on("renderitems", (renderedItems) => {
-        countItems();
-
-        // Step 1: Gather all unique attributes from the items
-        var uniqueAttributes = {}; // Using an object to store unique values
-
-        document.querySelectorAll(".summer-schools-item").forEach((item) => {
-          // Assuming 'fs-cmsfilter-field="Schwerpunkt"' is an attribute of a child element of '.summer-schools-item'
-          item.querySelectorAll('[fs-cmsfilter-field="Schwerpunkt"]').forEach((child) => {
-            var attributeValue = child.textContent.trim();
-            uniqueAttributes[attributeValue] = true; // Store the attribute value as a key in the object
-          });
-        });
-
-        // Step 2: Remove options from the select field that do not match the gathered attributes
-        document.querySelectorAll(".schwerpunkt-select option").forEach((option) => {
-          if (!uniqueAttributes[option.value]) {
-            // If the option value is not a key in our uniqueAttributes object, remove it
-            option.remove();
-          }
-        });
-      });
-    },
-  ]);
-
-  const tagsContainer = document.querySelector(".summer-schools_filter-tags");
-
-  // Listen for the pick event from the datepicker
-  datepickers.forEach((datepicker) => {
-    datepicker.addEventListener("pick.datepicker", (e) => {
-      let pickedDate = e.date;
-      filterItems(pickedDate);
-      setTimeout(() => {
-        countItems();
-        updateAndShowDateTag();
-      }, 200);
-    });
-
-    // Listen for input events
-    datepicker.addEventListener("input", () => {
-      let inputVal = datepicker.value;
-      // Regular expression to match date format DD.MM.YYYY
-      let dateRegex = /^(\d{2})\.(\d{2})\.(\d{4})$/;
-
-      if (dateRegex.test(inputVal)) {
-        let parsedDate = parseDate(inputVal);
-        // Trigger the filtering function if the date format is correct
-        filterItems(parsedDate);
-      }
-    });
-  });
-
-  document.querySelector(".summer-schools_date-filter-tag .close-tag").addEventListener("click", () => {
-    removeDateFilter();
-    setTimeout(() => {
-      countItems();
-    }, 200);
-  });
-
-  // Remove date filter when all filters are cleared
-  const clearFilters = document.querySelector(".summer-schools_clear-all-filters");
-  if (clearFilters) {
-    clearFilters.addEventListener("click", () => {
-      removeDateFilter();
-      removeAgeFilter();
-      setTimeout(() => {
-        countItems();
-      }, 200);
-    });
+    resetItemsVisibility()
   }
+
   // Event listener for the select field
   document.querySelector(".age-select").addEventListener("change", (event) => {
     var selectedAge = event.target.value;
@@ -215,22 +63,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (selectedAge) {
       // Call the function and pass the selected value
       filterAge(selectedAge);
-      updateAndShowAgeTag(selectedAge);
-      setTimeout(() => {
-        countItems();
-      }, 200);
     } else {
       removeAgeFilter();
     }
   });
 
-  // Reset age filter
-  document.querySelector(".summer-schools_age-filter-tag .close-tag").addEventListener("click", () => {
-    removeAgeFilter();
-    setTimeout(() => {
-      countItems();
-    }, 200);
-  });
 
   const schwerpunktDropdown = document.querySelector(".schwerpunkt-dropdown");
   schwerpunktDropdown[0].value = "";
@@ -283,6 +120,10 @@ document.addEventListener("DOMContentLoaded", () => {
   landSelect.addEventListener("input", () => {
     landInputHelper.value = landSelect.value;
     landInputHelper.dispatchEvent(new Event("input", { bubbles: true }));
+    var selectedAge = document.querySelector(".age-select").value;
+    if (selectedAge) {
+      filterAge(selectedAge);
+    }
   });
 
   const schwerpunktHelper = document.querySelector(".schwerpunkt-helper");
